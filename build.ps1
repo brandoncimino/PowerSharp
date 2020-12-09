98dcae2db858c551582d512b93f6b7623d4f93d2
Brandon Cimino <bcimino@cccis.com>
2020-12-09 15:45:57 -0600

    Added the full build.ps1

--- build.ps1
+++ build.ps1
@@ -1,1 +1,25 @@
 # This is the "build" script for the entire module, which combines both C# and PowerShell "modules"
+
+Remove-Item "$PSScriptRoot/build" -Recurse -Force
+
+#region Variables
+$module = "PowerSharp"
+$build_dir = New-Item -ItemType Directory "$PSScriptRoot/build/$module"
+$power_dir = Get-Item "$PSScriptRoot/power"
+$sharp_dir = Get-Item "$PSScriptRoot/sharp"
+$power_out = New-Item -ItemType Directory "$build_dir/power"
+$sharp_out = "$build_dir/bin"
+#endregion
+
+#region Move the manifest (.psd1) file
+$manifest = Get-Item "$PSScriptRoot/$module.psd1"
+Copy-Item -Path $manifest -Destination $build_dir
+#endregion
+
+#region Move the PowerShell module files
+Copy-Item -Path $power_dir -Destination $power_out -Recurse
+#endregion
+
+#region Build the C# .dll
+dotnet build $sharp_dir -o $sharp_out
+#endregion

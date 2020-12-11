99e284f2172a38d275050296f7f1aef5671a9217
brandoncimino <brandon.cimino@gmail.com>
2020-12-10 22:02:44 -0600

    Added better error handling and messageing to build.ps1

--- build.ps1
+++ build.ps1
@@ -1,6 +1,18 @@
 # This is the "build" script for the entire module, which combines both C# and PowerShell "modules"
 
-Remove-Item "$PSScriptRoot/build" -Recurse -Force -ErrorAction SilentlyContinue
+$old_build_dir = Get-Item "$PSScriptRoot/build" -ErrorAction SilentlyContinue
+if ($old_build_dir) {
+    try {
+        Remove-Item $old_build_dir -Recurse -Force -ErrorAction Stop
+    } catch [System.UnauthorizedAccessException] {
+        $_.ErrorDetails = "Couldn't remove $old_build_dir.
+It is likely loaded into the current PowerShell session, locking access to it.
+The only way to unload it (as far as I know) is to completely exit the session.
+See https://docs.microsoft.com/en-us/powershell/scripting/dev-cross-plat/create-standard-library-binary-module?view=powershell-7.1#important-details"
+        $_.Exception.HelpLink = 'https://docs.microsoft.com/en-us/powershell/scripting/dev-cross-plat/create-standard-library-binary-module?view=powershell-7.1#important-details'
+        throw $_
+    }
+}
 
 #region Variables
 $module = "PowerSharp"
@@ -9,6 +21,14 @@ $power_dir = Get-Item "$PSScriptRoot/power"
 $sharp_dir = Get-Item "$PSScriptRoot/sharp"
 $power_out = New-Item -ItemType Directory "$build_dir/power"
 $sharp_out = "$build_dir/bin"
+
+[ordered]@{
+    build_dir = $build_dir
+    power_dir = $power_dir
+    sharp_dir = $sharp_dir
+    power_out = $power_out
+    sharp_out = $sharp_out
+} | Out-String | Write-Host -ForegroundColor darkgray
 #endregion
 
 #region Move the manifest (.psd1) file

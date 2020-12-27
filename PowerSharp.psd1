07a5cf5547b58eaa44ddfd5d075073821e428e1d
brandoncimino <brandon.cimino@gmail.com>
2020-12-27 11:04:11 -0600

    Added PropertyUtils.psm1 nested module

--- PowerSharp.psd1
+++ PowerSharp.psd1
@@ -68,6 +68,7 @@
     # Modules to import as nested modules of the module specified in RootModule/ModuleToProcess
     NestedModules = @(
         "./bin/PowerSharp.dll"
+        "./power/PropertyUtils.psm1"
     )
 
     # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
---
+++ power/PropertyUtils.psm1
@@ -0,0 +1,75 @@
+$DefaultPrimaryKeys = "id", "name"
+$PrimaryKeyName = "PrimaryKey";
+
+function Add-PrimaryKey(
+    [Parameter(ValueFromPipeline)]
+    $Object,
+
+    [ValidateNotNullOrEmpty]
+    [string[]]$Property = $DefaultPrimaryKeys,
+
+    [switch]$IncludeDefaults
+) {
+    PROCESS {
+        if ($IncludeDefaults) {
+            $Property += $DefaultPrimaryKeys
+        }
+            
+        $Property = $Property | Select-Object -Unique
+
+        $Object | Add-AliasCascade -Alias $PrimaryKeyName -Property $Property
+    }
+}
+
+function Add-Alias(
+    [Parameter(ValueFromPipeline)]
+    $Object,
+
+    [Parameter(Mandatory)]
+    [string]$Alias,
+
+    [Parameter(Mandatory)]
+    [Alias("Properties")]
+    [string[]]$Property
+) {
+    PROCESS {
+        Write-Host -ForegroundColor DarkGray "Checking for the following properties to alias as $Alias, in order: [$Property]"
+
+        foreach ($prop in $Property) {
+            $member = $Object | Get-Member -Name $prop
+
+            switch ($member.Count) {
+                1 {
+                    Write-Host -ForegroundColor Blue "Found the property '$prop'"
+                    $Object | Add-Member -MemberType AliasProperty -Name $Alias -Value $Property
+                    return
+                }
+
+                { $_ -gt 1 } {
+                    throw "Found $($member.Count) properties satisfying the name '$prop': [$($member.Name)]. Please specify a more specific name."
+                }
+
+                0 {
+                    Write-Host -ForegroundColor DarkGray "Didn't find the property '$prop', continuing..."
+                }
+            }
+        }
+
+        throw "None of the specified properties were available to alias as '${Alias}': [$Property]"
+    }
+}
+
+function Add-PropertyCascade(
+    [Parameter(ValueFromPipeline)]
+    $Object,
+
+    [Parameter(Mandatory)]
+    [string]$Alias,
+
+    [Parameter(Mandatory)]
+    [object[]]$Getters
+) {
+    PROCESS {
+        throw [System.NotImplementedException]::new()
+    }
+}

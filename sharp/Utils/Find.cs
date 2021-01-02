aa822962e1190cf3494f1e61855ab83a60d9d4d0
brandoncimino <brandon.cimino@gmail.com>
2021-01-02 12:23:02 -0600

    Prefixed the `Find` methods with `First` for clarity Added methods to retrieve property and field values via reflection

--- sharp/Utils/Find.cs
+++ sharp/Utils/Find.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Management.Automation;
 using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace PowerSharp
 {
@@ -18,12 +21,35 @@ namespace PowerSharp
             return options.FirstOrDefault();
         }
 
-        public static T NonNull<T>(params T[] options){
+        public static T FirstNonNull<T>(params T[] options){
             return options.FirstOrDefault(it => it!=null);
         }
 
-        public static string NonBlank(params string[] options){
+        public static T FirstNonEmpty<T>(params T[] options){
+            return options.FirstOrDefault(it => it.IsNotEmpty());
+        }
+
+        public static string FirstNonBlank(params string[] options){
             return options.FirstOrDefault(it => !string.IsNullOrWhiteSpace(it));
+        }
+
+        public static object FirstNonEmptyProperty<T>(this IEnumerable<T> stuff, PropertyInfo property){
+            return stuff.Select(thing => property.GetValue(thing)).FirstOrDefault(value => value.IsNotEmpty());
+        }
+
+        public static object FirstNonEmptyField<T>(this IEnumerable<T> stuff, FieldInfo field){
+            return stuff.Select(thing => field.GetValue(thing)).FirstOrDefault(value => value.IsNotEmpty());
+        }
+
+        public static object FirstNonEmptyVariable<T>(this IEnumerable<T> stuff, MemberInfo variableMember){
+            switch(variableMember){
+                case FieldInfo field:
+                    return FirstNonEmptyField(stuff,field);
+                case PropertyInfo property:
+                    return FirstNonEmptyProperty(stuff,property);
+                default:
+                    throw new ArgumentException($"Please supply a 'variable', i.e. a {nameof(PropertyInfo)} or {nameof(FieldInfo)}");
+            }
         }
     }
 }
